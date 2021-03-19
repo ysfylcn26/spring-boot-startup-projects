697399e438d7f9d25f5af56205e7dd204942ee64
yusuf <yusuf@dekatechs.com>
2021-03-19 15:51:58 +0300

    cache role

--- parent-modul/models/src/main/java/com/core/model/repository/RoleRepository.java
+++ parent-modul/models/src/main/java/com/core/model/repository/RoleRepository.java
@@ -2,6 +2,7 @@ package com.core.model.repository;
 
 import com.core.model.models.entity.Role;
 import com.core.model.models.enums.RoleType;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.Set;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+    @Cacheable("role_cache_list")
     Optional<Set<Role>> findAllByRoleNameIn(Set<RoleType> roleTypes);
 
 }
